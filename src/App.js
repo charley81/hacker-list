c04fb587d2e34907d6b1fd74ba246f113cf33230
ChrisKnowsDev <chrisharley81@gmail.com>
2020-12-23 12:04:41 -0500

    emotion css setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+/** @jsxImportSource @emotion/react */
 import React, {useState, useEffect, useRef, useCallback} from 'react'
 import axios from 'axios'
+import { css, jsx } from '@emotion/react'
+
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" css={css`
+      background: red;
+    `}>
       <h1>Hacker List</h1>
     </div>
   );
